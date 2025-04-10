de79781e04fa395f86c01e284109ba94e1088b00
ma-muktadeer <muktadeer.ma@gmail.com>
2025-04-10 11:29:01 +0600

    ok

--- src/app/plt-mp/plt-mp.component.ts
+++ src/app/plt-mp/plt-mp.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MapService, MarkerData } from '../service/map.service';
 
 @Component({
   selector: 'app-plt-mp',
@@ -8,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class PltMpComponent {
 
+  mapService = inject(MapService);
+
+  sampleData: MarkerData[] = [
+    {
+      coordinates: [90.3563, 23.6850],
+      properties: {
+        title: "Dhaka Center",
+        description: "Capital of Bangladesh",
+        type: "city"
+      }
+    },
+    {
+      coordinates: [90.3832, 23.8103],
+      properties: {
+        title: "Important Location",
+        description: "Sample point of interest",
+        type: "poi"
+      }
+    }
+  ];
+
+  ngOnInit(): void {
+    this.mapService.initMap('map-container', [90.3563, 23.6850], 5, this.sampleData); //[90.3563, 23.6850]
+
+  }
+
+  ngOnDestroy(): void {
+    this.mapService.removeMarkers();
+  }
+
+  getMarkers() {
+    const markers = this.mapService.getMarkers();
+    console.log('markers are =>', markers);
+    return markers;
+  }
 }
--- tile-server.js
+++ tile-server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
 app.use(cors()); 
 app.use(compression()); 
